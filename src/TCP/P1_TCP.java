2f7506c935d38331584794d4a7934f5782bf4771
Leonardo de Jesus <leonardo2013ufs@gmail.com>
2016-07-21 00:20:57 -0300

    chat radio com o protocolo TCP

--- src/TCP/P1_TCP.java
+++ src/TCP/P1_TCP.java
@@ -5,27 +5,41 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class P1_TCP {
 
-	public static void main(String[] args) throws UnknownHostException, IOException {
-		// Representa as primitivas SOCKET , BIND E CONNECT SÃO FEITOS AO MESMO TEMPO
+	public static void main(String[] args) throws UnknownHostException,
+			IOException {
+		// Representa as primitivas SOCKET , BIND E CONNECT SÃO FEITOS AO MESMO
+		// TEMPO
 		Socket socket = new Socket("localhost", 300);
-		
-	   // em relação ao processo inputstream  
-		InputStream in = socket.getInputStream();//receb dados no processo
+
+		// em relação ao processo inputstream e outputinstream
+		InputStream in = socket.getInputStream();// receb dados no processo
 		OutputStream out = socket.getOutputStream();// envia dados do processo
+		// mecanismo para terminar o loop de funcionamento do chat de radio
+		String msg, msg2, off;
+		off = "desligar";
+		msg = "";
+		msg2 = "";
 		
-		String msg = "tudo bem";
-		byte [] buf = msg.getBytes();
-		out.write(buf);
-		
-		
-		
-		byte[] buf2 = new byte[20];
-		int size = in.read(buf2); //bloqueante
-		String msg2 = new String (buf2,0,size);
-		System.out.println(msg2);
+		System.out.println("********************** Terminal P1 *****************************\n");
+		//chat roda até a pessoa enviar desligar ou receber do outro processo a mesma palavra
+		while (!((msg.equals(off)) || (msg2.equals(off)))) {
+			// Parte de enviar mensagem
+			Scanner read = new Scanner(System.in);
+			System.out.print("Eu:");
+			msg = read.nextLine();
+			byte[] buf = msg.getBytes();
+			out.write(buf);
+			
+			// Parte de receber mensagem 
+			byte[] buf2 = new byte[20];
+			int size = in.read(buf2); // bloqueante
+			msg2 = new String(buf2, 0, size);
+			System.out.println("P2:" + msg2);
+		}
 		socket.close();
 
 	}
--- src/TCP/P2_TCP.java
+++ src/TCP/P2_TCP.java
@@ -5,28 +5,43 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class P2_TCP {
 
 	public static void main(String[] args) throws IOException {
-		//Aqui o lado servidor realiza no socket ativando 3 primitivas SOCKET/ BIND/LIST
+		// Aqui o lado servidor realiza no socket ativando 3 primitivas SOCKET/
+		// BIND/LIST
 		ServerSocket ssocket = new ServerSocket(300);
-		Socket socket = ssocket.accept(); //bloqueante
-		InputStream in = socket.getInputStream();//receb dados no processo
+		Socket socket = ssocket.accept(); // bloqueante
+		InputStream in = socket.getInputStream();// receb dados no processo
 		OutputStream out = socket.getOutputStream();// envia dados do processo
 		
-		byte[] buf = new byte[20];
-		int size = in.read(buf); //bloqueante
-		String msg = new String (buf,0,size);
-		System.out.println(msg);
+		// mecanismo para terminar o loop de funcionamento do chat de radio
+		String msg, msg2, off;
+		off = "desligar";
+		msg = "";
+		msg2 = "";
 		
-		
-		String msg2 = "Olá";
-		byte [] buf2 = msg2.getBytes();
-		out.write(buf2);
-		
+		System.out.println("********************** Terminal P2 *****************************\n");
+		//chat roda até a pessoa enviar desligar ou receber do outro processo a mesma palavra
+		while (!((msg.equals(off)) || (msg2.equals(off)))) {
+			// Parte de receber mensagem 
+			byte[] buf = new byte[20];
+			int size = in.read(buf); // bloqueante
+		    msg = new String(buf, 0, size);
+			System.out.println("P1:" + msg);
+
+			// Parte de enviar mensagem 
+			Scanner read = new Scanner(System.in);
+			System.out.print("Eu:");
+			msg2 = read.nextLine();
+
+			// String msg2 = "Olá";
+			byte[] buf2 = msg2.getBytes();
+			out.write(buf2);
+		}
 		socket.close();
-		
 
 	}
 
